2924bfc4b4e7b538d12d155142e23248b9bdf7bc
Shane Froebel <shane.froebel@gmail.com>
2017-06-06 16:36:31 -0400

    more building of the Patreon plugin

---
+++ Patreon.hooks.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * @ingroup Extensions
+ * @author Shane A. Froebel
+ * @copyright ©2017
+ * @license MIT
+ */
+class PatreonHooks
+{
+
+	/**
+	 * Adds hooks and adjusts the config once MediaWiki is fully initialised
+	 */
+	public static function initialize()
+	{
+		global $wgHooks;
+	}
+
+}
---
+++ Patreon.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Patreon API Services
+ * Adds the ability to link your MediaWiki users to their Patreon Accounts
+ * to give them certain rights and priviliges for your MediaWiki Installation.
+ *
+ * @license          MIT
+ * @package          Patreon
+ * @link               https://github.com/FrakMedia/mediawiki-patreon
+ *
+ **/
+
+if (function_exists('wfLoadExtension')) {
+	wfLoadExtension('Patreon');
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Patreon'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for Patreon extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die('This version of the Patreon extension requires MediaWiki 1.25+');
+}
+
+?>
